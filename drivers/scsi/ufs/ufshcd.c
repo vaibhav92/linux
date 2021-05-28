ce6198c2be6761b85f821f3971e4cbc5cee47e19
Alice.Chao <alice.chao@mediatek.com>
2021-05-28 11:36:21 +0800

    scsi: ufs: core: Export ufshcd_hba_stop()
    Export ufshcd_hba_stop() to allow vendors to disable HCI in variant ops.
    
    Link: https://lore.kernel.org/r/20210528033624.12170-2-alice.chao@mediatek.com
    Reviewed-by: Stanley Chu <stanley.chu@mediatek.com>
    Signed-off-by: Alice.Chao <alice.chao@mediatek.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -4495,7 +4495,7 @@ EXPORT_SYMBOL_GPL(ufshcd_make_hba_operational);
  * ufshcd_hba_stop - Send controller to reset state
  * @hba: per adapter instance
  */
-static inline void ufshcd_hba_stop(struct ufs_hba *hba)
+void ufshcd_hba_stop(struct ufs_hba *hba)
 {
 	unsigned long flags;
 	int err;
@@ -4514,6 +4514,7 @@ static inline void ufshcd_hba_stop(struct ufs_hba *hba)
 	if (err)
 		dev_err(hba->dev, "%s: Controller disable failed\n", __func__);
 }
+EXPORT_SYMBOL_GPL(ufshcd_hba_stop);
 
 /**
  * ufshcd_hba_execute_hce - initialize the controller
